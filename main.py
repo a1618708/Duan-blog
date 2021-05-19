16ca373ef21789541047fb38221504619746bb04
Duan <kusumia@hotmail.com>
2021-05-19 21:23:09 +0900

    Modify environment variables

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ from flask_gravatar import Gravatar
 from functools import wraps
 from flask import abort
 from sqlalchemy.orm import relationship
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 ckeditor = CKEditor(app)
 Bootstrap(app)
 login_manager = LoginManager()
