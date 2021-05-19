9a72eb6ceb7de789adb6a495f186b3fc3cb54b91
Duan <kusumia@hotmail.com>
2021-05-19 21:52:35 +0900

    Modify requirements

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def load_user(user_id):
 
 ##CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =False
 db = SQLAlchemy(app)
 
 ##CONFIGURE TABLES
